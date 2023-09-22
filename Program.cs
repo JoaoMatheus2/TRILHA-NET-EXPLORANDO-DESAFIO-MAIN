1f5b6da03d537acdcd7e0ad91d6d05318fd64bb7
JoaoMatheus2 <joaomatheus696@outlook.com>
2023-09-22 11:15:25 -0300

    On branch main  Initial commit  Changes to be committed: 	new file:   .gitignore 	new file:   DesafioProjetoHospedagem.csproj 	new file:   Models/Pessoa.cs 	new file:   Models/Reserva.cs 	new file:   Models/Suite.cs 	new file:   Program.cs 	new file:   README.md 	new file:   diagrama_classe_hotel.png

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using DesafioProjetoHospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p3 = new Pessoa(nome: "Hóspede 3");
+Pessoa p4 = new Pessoa(nome: "Hóspede 4");
+Pessoa p5 = new Pessoa(nome: "Hóspede 5");
+Pessoa p6 = new Pessoa(nome: "Hóspede 6");
+Pessoa p7 = new Pessoa(nome: "Hóspede 7");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+hospedes.Add(p3);
+hospedes.Add(p4);
+hospedes.Add(p5);
+hospedes.Add(p6);
+hospedes.Add(p7);
+
+// Cria a suíte
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 8, valorDiaria: 150);
+
+// Cria uma nova reserva, passando a suíte e os hóspedes
+Reserva reserva = new Reserva(diasReservados: 9);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
